cd195c9b15949b85e6097c73fe8c6d69321cec09
Misael <missael133@gmail.com>
2019-03-20 12:47:07 -0600

    DDA and bresenham work properly

--- Drawn Of Elipse DDA And Bresenham/Form1.Designer.cs
+++ Drawn Of Elipse DDA And Bresenham/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_TimeBresenham = new System.Windows.Forms.Label();
+            this.lbl_TimeDDA = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto Lt", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DDA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto Lt", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(365, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bresenham";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto Lt", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(186, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tiempo De Ejecución";
+            // 
+            // lbl_TimeBresenham
+            // 
+            this.lbl_TimeBresenham.AutoSize = true;
+            this.lbl_TimeBresenham.Font = new System.Drawing.Font("Roboto Lt", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TimeBresenham.Location = new System.Drawing.Point(284, 59);
+            this.lbl_TimeBresenham.Name = "lbl_TimeBresenham";
+            this.lbl_TimeBresenham.Size = new System.Drawing.Size(39, 13);
+            this.lbl_TimeBresenham.TabIndex = 4;
+            this.lbl_TimeBresenham.Text = "00 : 00";
+            // 
+            // lbl_TimeDDA
+            // 
+            this.lbl_TimeDDA.AutoSize = true;
+            this.lbl_TimeDDA.Font = new System.Drawing.Font("Roboto Lt", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TimeDDA.Location = new System.Drawing.Point(165, 59);
+            this.lbl_TimeDDA.Name = "lbl_TimeDDA";
+            this.lbl_TimeDDA.Size = new System.Drawing.Size(39, 13);
+            this.lbl_TimeDDA.TabIndex = 5;
+            this.lbl_TimeDDA.Text = "00 : 00";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox.Location = new System.Drawing.Point(12, 96);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(470, 345);
+            this.pictureBox.TabIndex = 6;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Roboto Lt", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(168, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(494, 491);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.lbl_TimeDDA);
+            this.Controls.Add(this.lbl_TimeBresenham);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_TimeBresenham;
+        private System.Windows.Forms.Label lbl_TimeDDA;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Drawn Of Elipse DDA And Bresenham/Form1.cs
+++ Drawn Of Elipse DDA And Bresenham/Form1.cs
@@ -7,14 +7,234 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+
 
 namespace Drawn_Of_Elipse_DDA_And_Bresenham
 {
     public partial class Form1 : Form
     {
+        const int width = 470;
+        const int height = 345;
+        Bitmap bmp = new Bitmap(width, height);
+
+        Point Punto_C;
+        Point Punto_R;
+        
+        int xc, yc = 0;
+        int xr, yr = 0;
+        int Rx, Ry;
+        int flag = 0;
+
+
         public Form1()
         {
+            
             InitializeComponent();
         }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (flag == 0)
+            {
+                xc = e.X;
+                yc = e.Y;
+
+                Punto_C = new Point((int)xc, (int)yc);
+                flag++;
+            }
+            else
+            {
+                
+                xr = e.X;
+                yr = e.Y;
+
+                Punto_R = new Point((int)xr, (int)yr);
+
+                GetRadio(xc,yc,xr,yr);
+                DrawnCoordenada();
+                //ElipseDDA(Rx, Ry);
+               ElipseBresenham(Rx, Ry);
+
+                flag = 0;
+            }
+            
+        }
+
+        private void GetRadio(int xc,int yc,int xr,int yr)
+        {
+            Graphics Gfx_C = Graphics.FromImage(bmp);
+            int radio1x, radio1y, radio2_x, radio2_y; 
+            radio1x =xc;
+            radio1y = yr;
+            radio2_x = xr;
+            radio2_y = yc;
+
+            Rx = Math.Abs(xr -xc);
+            Ry = Math.Abs(yc - yr);
+
+            Gfx_C.FillRectangle(Brushes.Blue, radio1x, radio1y, 2, 2);
+            Gfx_C.FillRectangle(Brushes.Black, radio2_x, radio2_y, 2, 2);
+        }
+        private void DrawnQuadrant(int xc, int yc, int x, int y, Color col)
+        {
+
+            if (x + xc > 0 && x + xc < width && y + yc > 0 && y + yc < height)  /// Cuadrante 2
+                bmp.SetPixel((int)x + xc, (int)y + yc, col);
+            if (-x + xc > 0 && -x + xc < width && -y + yc > 0 && -y + yc < height)  ///CUADRANTE 6
+                bmp.SetPixel((int)-x + xc, (int)-y + yc, col);
+            if (-x + xc > 0 && -x + xc < width && y + yc > 0 && y + yc < height)  ///Cuadrante 3
+                bmp.SetPixel((int)-x + xc, (int)y + yc, col);
+            if (x + xc > 0 && x + xc < width && -y + yc > 0 && -y + yc < height)  ///Cuadrante 7
+                bmp.SetPixel((int)x + xc, (int)-y + yc, col);
+            /*
+            setPixel(xc + x, yc + y);
+            setPixel(xc - x, yc + y);
+            setPixel(xc + x, yc - y);
+            setPixel(xc - x, yc - y);
+            */
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillRectangle(Brushes.White, 0, 0, width, height);
+            pictureBox.Image = bmp;
+
+            lbl_TimeBresenham.Text = "00 :00";
+            lbl_TimeDDA.Text = "00 : 00";
+        }
+
+        private void DrawnCoordenada()
+        {
+            int xc, yc, xr, yr;
+            Graphics Gfx = Graphics.FromImage(bmp);
+
+            xr = Punto_R.X;
+            yr = Punto_R.Y;
+
+            xc = Punto_C.X;
+            yc = Punto_C.Y;
+
+            Gfx.FillRectangle(Brushes.Tomato, xr - 1, yr - 1, 2, 2);
+            Gfx.FillRectangle(Brushes.Tomato, xc - 1, yc - 1, 2, 2);
+            pictureBox.Image = bmp;
+
+        }
+
+        private void ElipseDDA(int Rx,int Ry)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            double x,y;
+           
+            for  (x = 0; x <= Rx; x++)
+            {
+                y = Math.Sqrt((((Math.Pow(Rx, 2)) * (Math.Pow(Ry, 2))) - ((Math.Pow(x, 2)) * (Math.Pow(Ry, 2)))) / Math.Pow(Rx, 2));
+
+
+                if (x + xc > 0 && x + xc < width && (int)y + yc > 0 && (int)y + yc < height)
+                    bmp.SetPixel((int)x + xc, (int)y + yc, Color.Cyan);
+
+                if (-x + xc > 0 && -x + xc < width && (int)y + yc > 0 && (int)y + yc < height)
+                    bmp.SetPixel((int)-x + xc, (int)y + yc, Color.Cyan);
+
+                if (-x + xc > 0 && -x + xc < width && -(int)y + yc > 0 && -(int)y + yc < height)
+                    bmp.SetPixel((int)-x + xc, -(int)y + yc, Color.Cyan);
+
+                 if (x + xc > 0 && x + xc < width && -(int)y + yc > 0 && -(int)y + yc < height)
+                    bmp.SetPixel((int)x + xc, -(int)y + yc, Color.Cyan);
+
+            }
+              for (y = 0; y <= Ry; y++)
+              {
+                  x = Math.Sqrt((((Math.Pow(Rx, 2)) * (Math.Pow(Ry, 2))) - ((Math.Pow(y, 2)) * (Math.Pow(Rx, 2)))) / Math.Pow(Ry, 2));
+
+                  if ((int)x + xc > 0 && (int)x + xc < width && (int)y + yc > 0 && (int)y + yc < height)
+                      bmp.SetPixel((int)x  + xc, (int)y + yc, Color.Cyan);
+
+                  if (-(int)x + xc > 0 && -(int)x + xc < width && -y + yc > 0 && -y + yc < height)
+                      bmp.SetPixel(-(int)x + xc, -(int)y + yc, Color.Cyan);
+
+                  if (-(int)x + xc > 0 && -(int)x + xc < width && y + yc > 0 && y + yc < height)
+                      bmp.SetPixel(-(int)x + xc, (int)y  + yc, Color.Cyan);
+
+                  if ((int)x + xc > 0 && (int)x + xc < width && (int)-y + yc > 0 && -y + yc < height)
+                     bmp.SetPixel((int)x + xc, -(int)y + yc, Color.Cyan);
+              }
+
+            lbl_TimeDDA.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
+        }
+
+        private void ElipseBresenham(int Rx, int Ry)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            double p1, p2;
+            int x = 0;
+            int y = Ry;
+            double rx2 = Math.Pow(Rx, 2);
+            double ry2 = Math.Pow(Ry, 2);
+            p1 = ry2 - (rx2 * Ry) + (0.25 * rx2);
+            while ((ry2 * x) < (rx2 * y))
+            {
+                if (p1 < 0)
+                {
+                    x++;
+                    p1 = p1 + (2 * ry2 * x) + ry2;
+                }
+                else
+                {
+                    x++;
+                    y--;
+                    p1 = p1 + (2 * ry2 * x) - (2 * rx2 * y) + ry2;
+                }
+
+                if ((int)x + xc > 0 && (int)x + xc < width && (int)y + yc > 0 && (int)y + yc < height)
+                    bmp.SetPixel((int)x + xc, y + yc, Color.Orange);
+
+                if (-x + xc > 0 && -x + xc < width && y + yc > 0 && y + yc < height)
+                    bmp.SetPixel(-x + xc, y + yc, Color.Orange);
+
+                if (-x + xc > 0 && -x + xc < width && -y + yc > 0 && -y + yc < height)
+                    bmp.SetPixel(-x + xc, -y + yc, Color.Orange);
+
+                if (x + xc > 0 && x + xc < width && -y + yc > 0 && -y + yc < height)
+                    bmp.SetPixel(x + xc, -y + yc, Color.Orange);
+            }
+            p2 = (ry2) * Math.Pow((x + 0.5), 2) + (rx2) * Math.Pow((y - 1), 2) - (rx2 * ry2);
+            while (y > 0)
+            {
+                if (p2 > 0)
+                {
+                    y--;
+                    p2 = p2 - (2 * rx2 * y) + rx2;
+                }
+                else
+                {
+                    x++;
+                    y--;
+                    p2 = p2 + (2 * ry2 * x) - (2 * rx2 * y) + rx2;
+                }
+
+                if (x + xc > 0 && x + xc < width && (int)y + yc > 0 && (int)y + yc < height)
+                    bmp.SetPixel(x + xc, y + yc, Color.Orange);
+
+                if (-x + xc > 0 && -x + xc < width && y + yc > 0 && y + yc < height)
+                    bmp.SetPixel(-x + xc, y + yc, Color.Orange);
+
+                if (-x + xc > 0 && -x + xc < width && -y + yc > 0 && -y + yc < height)
+                    bmp.SetPixel(-x + xc, -y + yc, Color.Orange);
+
+                if (x + xc > 0 && x + xc < width && -y + yc > 0 && -y + yc < height)
+                    bmp.SetPixel(x + xc, -y + yc, Color.Orange);
+                
+            }
+            lbl_TimeBresenham.Text = String.Format("{0}", sw.Elapsed.TotalMilliseconds);
+        }
+        
     }
 }
+
+
